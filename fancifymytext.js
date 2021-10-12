0e71dfd00e9d53b1239112ea67e4890bff4db91a
DoctorSmoogy <79550103+DoctorSmoogy@users.noreply.github.com>
2021-10-11 17:19:50 -0700

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,33 +1,32 @@
 function popUpAlert() {
     alert("Hello, world!");
-    document.getElementById("text").style.fontSize = "24pt";
+    document.getElementById("text").style.fontSize = "2em";
 }
 
 function radioButton() {
-    alert("Radio Button Changed!");
-    var x = document.querySelector('input[name="radioButton"]:checked');
-    if(x.id == "fancy") {
+    alert("HEAVEN OR HELL LET'S ROCK");
+    if(document.getElementById('fancySchmancy').checked) {
         document.getElementById("text").style.fontWeight = "bold";
         document.getElementById("text").style.color = "blue";
         document.getElementById("text").style.textDecoration = "underline";
     }
-    else if(x.id == "boring") {
+    else if(document.getElementById('boringBetty').checked) {
         document.getElementById("text").style.removeProperty("font-weight");
         document.getElementById("text").style.removeProperty("color");
         document.getElementById("text").style.removeProperty("text-decoration");
     }
 }
 
-function mooButton() {
+function moo() {
     document.getElementById("text").style.textTransform = "uppercase";
     var str = document.getElementById("text").value;
-    var arr = str.split("");
+    var parts = str.split("");
     var i;
-    for(i = 0; i < arr.length; i++) {
-        if(arr[i] == ".") {
-            arr[i] = "Moo.";
+    for(i = 0; i < parts.length; i++) {
+        if(parts[i] == ".") {
+            parts[i] = "-Moo.";
         }
     }
-    str = arr.join("");
+    str = parts.join("");
     document.getElementById("text").value = str;
 }
